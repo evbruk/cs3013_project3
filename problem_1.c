0677311236b359075bea0b49ae4707aa989594b4
Noah Parker <nparker@wpi.edu>
2020-02-17 12:27:55 -0500

    Adding some cats and dogs.

--- problem_1.c
+++ problem_1.c
@@ -2,13 +2,33 @@
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
+#include <pthread.h>
 #define NUM_DOGS 9
 #define NUM_CATS 13
 
 int main(int argc, char * argv[])
 {
 	printf("Creating %d dogs & %d cats... \n", NUM_DOGS, NUM_CATS);
+	//cats	
+	pthread_t chico_cat;
+	pthread_t mittens_cat;
+	pthread_t pickles_cat;
+	pthread_t cake_cate;
+	
+	//dogs
 	
 
+
+}
+
+void dog(char * dogName)
+{
+	printf("Dog %s created! \n", dogName);
+
+}
+
+void cat(char * catName)
+{
+	printf("Cat %s created! \n", catName);
 
 }
