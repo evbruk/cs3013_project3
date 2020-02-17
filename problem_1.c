4fb6c32b74608a1b3995e2a5d7f8f12ca7bddbfe
Noah Parker <nparker@wpi.edu>
2020-02-17 12:17:48 -0500

    Adding problem_1.c file with some lite starter code.

---
+++ problem_1.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#define NUM_DOGS 9
+#define NUM_CATS 13
+
+int main(int argc, char * argv[])
+{
+	printf("Creating %d dogs & %d cats... \n", NUM_DOGS, NUM_CATS);
+	
+
+
+}
