4051a4e45f6f7210b1ee12ae4fd0f0376746b9fc
Eva Bruklich <eva.bruklich@gmail.com>
2020-02-17 12:49:00 -0500

    created dogs and cats

--- problem_1.c
+++ problem_1.c
@@ -6,21 +6,6 @@
 #define NUM_DOGS 9
 #define NUM_CATS 13
 
-int main(int argc, char * argv[])
-{
-	printf("Creating %d dogs & %d cats... \n", NUM_DOGS, NUM_CATS);
-	//cats	
-	pthread_t chico_cat;
-	pthread_t mittens_cat;
-	pthread_t pickles_cat;
-	pthread_t cake_cate;
-	
-	//dogs
-	
-
-
-}
-
 void dog(char * dogName)
 {
 	printf("Dog %s created! \n", dogName);
@@ -32,3 +17,62 @@ void cat(char * catName)
 	printf("Cat %s created! \n", catName);
 
 }
+
+int main(int argc, char * argv[])
+{
+	printf("Creating %d dogs & %d cats... \n", NUM_DOGS, NUM_CATS);
+
+	//cats	
+	pthread_t chico_cat;
+	pthread_t mittens_cat;
+	pthread_t pickles_cat;
+	pthread_t cake_cat;
+	pthread_t cheeks_cat;
+	pthread_t mozz_cat
+	pthread_t soda_cat;
+	pthread_t kitten_cat;
+	pthread_t luna_cat;
+	pthread_t violet_cat;
+	pthread_t honeymust_cat;
+	pthread_t buster_cat;
+	pthread_t munchkin_cat;
+
+	//create cats
+	pthread_create(&chico_cat, NULL, cat, "Chico");
+	pthread_create(&mittens_cat, NULL, cat, "Mittens");
+	pthread_create(&pickles_cat, NULL, cat, "Pickles");
+	pthread_create(&cake_cat, NULL, cat, "Cake");
+	pthread_create(&cheeks_cat, NULL, cat, "Cheeks");
+	pthread_create(&mozz_cat, NULL, cat, "Mozz");
+	pthread_create(&soda_cat, NULL, cat, "Soda");
+	pthread_create(&kitten_cat, NULL, cat, "Kitten");
+	pthread_create(&luna_cat, NULL, cat, "Luna");
+	pthread_create(&violet_cat, NULL, cat, "Violet");
+	pthread_create(&honeymust_cat, NULL, cat, "Honeymust");
+	pthread_create(&buster_cat, NULL, cat, "Buster");
+	pthread_create(&munchkin_cat, NULL, cat, "Munchkin");
+	
+	//dogs
+	pthread_t archie_dog;
+	pthread_t toby_dog;
+	pthread_t brady_dog;
+	pthread_t tom_dog;
+	pthread_t buddy_dog;
+	pthread_t princess_dog;
+	pthread_t rosie_dog;
+	pthread_t yeller_dog;
+	pthread_t brandy_dog;	
+
+	//create dogs
+	pthread_create(&archie_dog, NULL, dog, "Archie");
+	pthread_create(&toby_dog, NULL, dog, "Toby");
+	pthread_create(&brady_dog, NULL, dog, "Brady");
+	pthread_create(&tom_dog, NULL, dog, "Tom");
+	pthread_create(&buddy_dog, NULL, dog, "Buddy");
+	pthread_create(&princess_dog, NULL, dog, "Princess");
+	pthread_create(&rosie_dog, NULL, dog, "Rosie");
+	pthread_create(&yeller_dog, NULL, dog, "Yeller");
+	pthread_create(&brandy_dog, NULL, dog, "Brandy");
+	
+}
+
